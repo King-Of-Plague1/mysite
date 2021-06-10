603f8f6dd880886a58a518e3b268d852560672b1
King-Of_Plague1 <korob228@bk.ru>
2021-06-10 17:40:08 +0500

    tests added

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
+    path('users/', views.UserList.as_view(), name='users_list'),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('cycles/', views.CycleList.as_view()),
     path('cycles/<int:pk>/', views.CycleDetail.as_view()),
